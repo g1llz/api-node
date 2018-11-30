0348d182e28bc5e7c7b8c48ff206eaeed34df8fb
Gilles Lima <gillestoim@gmail.com>
2018-11-29 22:00:45 -0200

    implementation of environment variables

--- src/services/payments/create-plan.js
+++ src/services/payments/create-plan.js
@@ -1,8 +1,16 @@
 const request = require('request');
+
+const psURL = process.env.PAG_url;
+const email = process.env.PAG_email;
+const token = process.env.PAG_token;
+
 const create = (name, amountPerPayment, reference) => {
     const options = {
-        headers: { 'Content-Type': 'application/json;charset=ISO-8859-1 ', 'Accept': 'application/vnd.pagseguro.com.br.v3+json;charset=ISO-8859-1' },
-        url: 'https://ws.sandbox.pagseguro.uol.com.br/pre-approvals/request?email=*.com&token=*',
+        headers: {
+            'Content-Type': 'application/json;charset=ISO-8859-1 ',
+            'Accept': 'application/vnd.pagseguro.com.br.v3+json;charset=ISO-8859-1'
+        },
+        url: `${psURL}/request?email=${email}&token=${token}`,
         body: JSON.stringify({
             reference: reference,
             preApproval: {
@@ -16,7 +24,7 @@ const create = (name, amountPerPayment, reference) => {
                 }
             },
             receiver: {
-                email: '*'
+                email: email
             }
         })
     }
--- src/services/payments/sign-plan.js
+++ src/services/payments/sign-plan.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
+
 const request = require('request');
+
+const psURL = process.env.PAG_url;
+const email = process.env.PAG_email;
+const token = process.env.PAG_token;
+
 const sign = (customer) => {
     const options = {
         headers: {
             'Content-Type': 'application/json;charset=ISO-8859-1 ',
             'Accept': 'application/vnd.pagseguro.com.br.v3+json;charset=ISO-8859-1'
         },
-        url: 'https://ws.sandbox.pagseguro.uol.com.br/pre-approvals?email=*&token=*',
+        url: `${psURL}/pre-approvals?email=${email}&token=${token}`,
         body: JSON.stringify({
             plan: customer.plan,
             reference: customer.reference,
