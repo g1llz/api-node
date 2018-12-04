3f4f9e9f17c3583af82b53cdff02ae80dd38b252
Gilles Lima <gillestoim@gmail.com>
2018-12-03 23:56:12 -0200

    add item in routes exclusions list

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const authGuard = require('../http/authGuard');
 
 const server = restify.createServer();
 
-const exclusions = ['/auth'];
+const exclusions = ['/auth', '/auth/register', '/plans'];
 
 server.pre(cors.preflight);
 server.use(cors.actual);
