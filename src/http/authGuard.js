f8e170348c34691be4bb5e1e7da6162cbf3efa9e
Gilles Lima <gillestoim@gmail.com>
2018-12-03 23:54:40 -0200

    auth and authguard update

--- src/http/authGuard.js
+++ src/http/authGuard.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authGuard = (deps) => {
     return (req, res, next) => {
+        const path = req.href().slice(1, 6);
         if (!deps.exclusions.includes(req.href())) {
             const token = req.headers['x-access-token'];
             if (!token) {
@@ -10,6 +11,12 @@ const authGuard = (deps) => {
             }
             try {
                 req.decoded = jwt.verify(token, process.env.JWT_secret);
+                if (path === 'admin') {
+                    if (req.decoded.role !== 'admin') {
+                        res.send(401, { error: 'Não autorizado.' });
+                        return false;
+                    }
+                }
             } catch (error) {
                 res.send(403, { error: 'Token inválido.' });
                 return false;
--- src/services/mysql/auth.js
+++ src/services/mysql/auth.js
@@ -6,13 +6,13 @@ const auth = deps => {
     return {
         authenticate: (email, password) => {
             return new Promise((resolve, reject) => {
-                connect.query('SELECT id, email FROM user WHERE email = ? AND password = ?', [email, sha1(password)], (error, results) => {
+                connect.query('SELECT id, email, role FROM user WHERE email = ? AND password = ?', [email, sha1(password)], (error, results) => {
                     if (error || !results.length) {
                         errorHandler(error, 'Falha ao localizar o usuários.', reject);
                         return false;
                     };
-                    const { email, id } = results[0];
-                    const token = jwt.sign({ email, id }, process.env.JWT_secret, { expiresIn: 60 * 60 * 24 });
+                    const { email, id, role } = results[0];
+                    const token = jwt.sign({ email, id, role }, process.env.JWT_secret, { expiresIn: 60 * 60 * 24 });
                     resolve({ token });
                 });
             })
