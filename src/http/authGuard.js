3a05392dce98587458bf9f83792b845fb30fb101
Gilles Lima <gillestoim@gmail.com>
2019-05-30 08:54:50 -0300

    change environment variables

--- src/http/authGuard.js
+++ src/http/authGuard.js
@@ -1,30 +1,30 @@
 const jwt = require('jsonwebtoken');
 
 const authGuard = (deps) => {
-    return (req, res, next) => {
-        /* const path = req.href().slice(8); */
-        if (!deps.exclusions.includes(req.href())) {
-            const token = req.headers['x-access-token'];
-            if (!token) {
-                res.send(403, { error: 'Token não fornecido.' });
+  return (req, res, next) => {
+    /* const path = req.href().slice(8); */
+    if (!deps.exclusions.includes(req.href())) {
+      const token = req.headers['x-access-token'];
+      if (!token) {
+        res.send(403, { error: 'Token não fornecido.' });
+        return false;
+      }
+      try {
+        req.decoded = jwt.verify(token, process.env.JWT_SECRET);
+        /* role verification */
+        /* if (path === 'admin') {
+            if (req.decoded.role !== 'admin') {
+                res.send(401, { error: 'Não autorizado.' });
                 return false;
             }
-            try {
-                req.decoded = jwt.verify(token, process.env.JWT_secret);
-                /* role verification */
-                /* if (path === 'admin') {
-                    if (req.decoded.role !== 'admin') {
-                        res.send(401, { error: 'Não autorizado.' });
-                        return false;
-                    }
-                } */
-            } catch (error) {
-                res.send(403, { error: 'Token inválido.' });
-                return false;
-            }
-        }
-        next();
+        } */
+      } catch (error) {
+        res.send(403, { error: 'Token inválido.' });
+        return false;
+      }
     }
+    next();
+  }
 }
 
 module.exports = authGuard;
--- src/services/mysql/auth.js
+++ src/services/mysql/auth.js
@@ -2,22 +2,22 @@ const sha1 = require('sha1');
 const jwt = require('jsonwebtoken');
 
 const auth = deps => {
-    const { connect, errorHandler } = deps;
-    return {
-        authenticate: (email, password) => {
-            return new Promise((resolve, reject) => {
-                connect.query('SELECT id, email, role FROM user WHERE email = ? AND password = ?', [email, sha1(password)], (error, results) => {
-                    if (error || !results.length) {
-                        errorHandler(error, 'Falha ao localizar o usuários.', reject);
-                        return false;
-                    };
-                    const { email, id, role } = results[0];
-                    const token = jwt.sign({ email, id, role }, process.env.JWT_secret, { expiresIn: 60 * 60 * 24 });
-                    resolve({ token });
-                });
-            })
-        }
+  const { connect, errorHandler } = deps;
+  return {
+    authenticate: (email, password) => {
+      return new Promise((resolve, reject) => {
+        connect.query('SELECT id, email, role FROM user WHERE email = ? AND password = ?', [email, sha1(password)], (error, results) => {
+          if (error || !results.length) {
+            errorHandler(error, 'Falha ao localizar o usuários.', reject);
+            return false;
+          };
+          const { email, id, role } = results[0];
+          const token = jwt.sign({ email, id, role }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+          resolve({ token });
+        });
+      })
     }
+  }
 }
 
 module.exports = auth;
