ebbf0fb7a2109c458d20de51bbfee82a72b356f2
Gilles Lima <gillestoim@gmail.com>
2019-05-30 08:55:17 -0300

    update routes

--- src/http/routes.js
+++ src/http/routes.js
@@ -1,49 +1,49 @@
 const db = require('../services/mysql');
 
 const routes = (server) => {
-    server.post(`${process.env.URI}/auth`, async (req, res, next) => {
-        const { email, password } = req.body;
-        try {
-            res.json(await db.auth().authenticate(email, password));
-        } catch (error) {
-            res.json(error);
-        }
-        next();
-    });
+  server.post(`${process.env.URI}/auth`, async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      res.json(await db.auth().authenticate(email, password));
+    } catch (error) {
+      res.json(error);
+    }
+    next();
+  });
 
-    server.get(`${process.env.URI}/users`, async (req, res, next) => {
-        try {
-            res.json(await db.users().all());
-        } catch (error) {
-            res.json(error);
-        }
-        next();
-    });
+  server.get(`${process.env.URI}/users`, async (req, res, next) => {
+    try {
+      res.json(await db.users().all());
+    } catch (error) {
+      res.json(error);
+    }
+    next();
+  });
 
-    server.post(`${process.env.URI}/users`, async (req, res, next) => {
-        const { email, password } = req.body;
-        try {
-            res.json(await db.users().save(email, password));
-        } catch (error) {
-            res.json(error);
-        }
-        next();
-    });
+  server.post(`${process.env.URI}/users`, async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      res.json(await db.users().save(email, password));
+    } catch (error) {
+      res.json(error);
+    }
+    next();
+  });
 
-    server.get(`${process.env.URI}/users/:id`, async (req, res, next) => {
-        const { id } = req.params;
-        try {
-            res.json(await db.users().byId(id));
-        } catch (error) {
-            res.json(error);
-        }
-        next();
-    });
+  server.get(`${process.env.URI}/users/:id`, async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      res.json(await db.users().byId(id));
+    } catch (error) {
+      res.json(error);
+    }
+    next();
+  });
 
-    server.get(process.env.URI, (req, res, next) => {
-        res.json({ text: 'silence is gold.' });
-        next();
-    });
+  server.get(process.env.URI, (req, res, next) => {
+    res.json({ text: 'silence is gold.' });
+    next();
+  });
 };
 
 module.exports = routes;
