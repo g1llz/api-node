a59388fbaf9cb06c716ddf6c728cee460a4f5a8e
Gilles Lima <gillestoim@gmail.com>
2020-01-27 15:32:18 -0200

    update check user role example

--- src/http/authGuard.js
+++ src/http/authGuard.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authGuard = (deps) => {
   return (req, res, next) => {
-    /* const path = req.href().slice(8); */
+    const adminRequired = !!req.href().match(/\/api\/v1\/admin\//g);
     if (!deps.exclusions.includes(req.href())) {
       const token = req.headers['x-access-token'];
       if (!token) {
@@ -11,13 +11,13 @@ const authGuard = (deps) => {
       }
       try {
         req.decoded = jwt.verify(token, process.env.JWT_SECRET);
-        /* role verification */
-        /* if (path === 'admin') {
-            if (req.decoded.role !== 'admin') {
-                res.send(401, { error: 'Não autorizado.' });
-                return false;
-            }
-        } */
+        // role verification example
+        if (adminRequired) {
+          if (req.decoded.role !== 'admin') {
+            res.send(401, { error: 'Não autorizado.' });
+            return false;
+          }
+        }
       } catch (error) {
         res.send(403, { error: 'Token inválido.' });
         return false;
