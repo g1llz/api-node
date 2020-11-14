dbf5c8eb679eb21dd7799b232cb2283503bcad98
Gilles Lima <gillestoim@gmail.com>
2020-11-14 18:55:37 +0000

    chrore: update project config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,  
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  parser: 'typescript',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  useTabs: false,
+}
