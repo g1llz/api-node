b101166487a453d52de2779d60306e25502a0a02
Gilles Lima <gillestoim@gmail.com>
2023-01-21 15:36:04 -0300

    feat: add more response methods

--- src/infra/adapters/CreateResponse.ts
+++ src/infra/adapters/CreateResponse.ts
@@ -4,4 +4,12 @@ export class CreateResponse {
   static ok(data?: Record<string, unknown>): HttpResponse {
     return { status: 200, data };
   }
+
+  static created(data?: Record<string, unknown>): HttpResponse {
+    return { status: 201, data };
+  }
+
+  static notFound(): HttpResponse {
+    return { status: 404 };
+  }
 }
