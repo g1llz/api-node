7ff4235b04738ebf63e3c18509c61028f8a83d07
Gilles Lima <gillestoim@gmail.com>
2023-01-21 13:41:42 -0300

    feat: ud routes; create api configs file

---
+++ src/config/api.ts
@@ -0,0 +1,8 @@
+export const API = {
+  v1: {
+    resources: {
+      auth: '/v1/auth',
+      user: '/v1/user',
+    },
+  }
+};
--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -1,27 +1,28 @@
 import { Application } from 'express';
+import { API } from '../../config/api';
 
-import AuthController from '../../presentation/controllers/auth';
-import UserController from '../../presentation/controllers/user';
-import TodoController from '../../presentation/controllers/todo';
+import { AuthController } from '../../presentation/controllers/AuthController';
+import { UserUpdateController } from '../../presentation/controllers/UserUpdateController';
+import { UserCreateController } from '../../presentation/controllers/UserCreateController';
+import { UserLoadController } from '../../presentation/controllers/UserLoadController';
+import { ExpressAdapter } from '../adapters/ExpressAdapter';
 
 import authGuard from './auth-guard';
+
+const {
+  resources: { auth: AUTH, user: USER },
+} = API.v1;
 
 export class Router {
   constructor(private readonly server: Application) {}
 
   setup() {
     // auth
-    this.server.post('/v1/login', AuthController.login);
+    this.server.post(AUTH, new ExpressAdapter(new AuthController()).adapt);
 
     // user
-    this.server.get('/user/:id', [UserController.show, authGuard]);
-    this.server.put('/user/:id', [UserController.update, authGuard]);
-    this.server.post('/user', UserController.store);
-
-    // todo
-    this.server.get('/todo', [TodoController.index, authGuard]);
-    this.server.get('/todo/:id', [TodoController.show, authGuard]);
-    this.server.put('/todo/:id', [TodoController.update, authGuard]);
-    this.server.post('/todo', [TodoController.store, authGuard]);
+    this.server.post(USER, new ExpressAdapter(new UserCreateController()).adapt);
+    this.server.get(`${USER}/:userId`, new ExpressAdapter(new UserLoadController()).adapt);
+    this.server.put(`${USER}/:userId`, new ExpressAdapter(new UserUpdateController()).adapt);
   }
 }
